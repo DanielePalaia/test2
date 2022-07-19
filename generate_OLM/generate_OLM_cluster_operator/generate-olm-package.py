b7bc2fe226b56ac18ea98754127969fccf2837cb
DanielePalaia <daniele985@gmail.com>
2022-07-19 13:43:32 +0200

    commit

--- generate_OLM/generate_OLM_cluster_operator/generate-olm-package.py
+++ generate_OLM/generate_OLM_cluster_operator/generate-olm-package.py
@@ -1,6 +1,11 @@
 import os
 import sys
 
+# insert at position 1 in the path, as 0 is the path of this file.
+sys.path.insert(1, '/Users/dpalaia/projects/test2/common_code')
+
+from create_overlay import create_overlay
+
 if len(sys.argv) == 2:
    print('the script needs at least a cluster operator manifests and version as arguments in order to create the OLM structure')
    sys.exit()
@@ -8,41 +13,10 @@ if len(sys.argv) == 2:
 operator_release_file = sys.argv[1] 
 version = sys.argv[2]
 
-# This function complete an overlay generator file (in ./generators) for Role, Clusterrole and Deployment
-def create_overlay(release_file, kind, firstString, endString, file_generator, file_output):
-
-    found = False
-    parsing = False
-
-
-    with open(release_file, 'r') as myfile:
-
-        filestring = ""
-
-        for line in myfile:
-            if parsing == True:
-    
-                if line.find(endString) >=0:
-                    if found == True:
-                        os.system("cp " + str(file_generator) + " " + str(file_output))
-                        with open(file_output, 'a') as myfile:
-                            myfile.write(filestring)
-                            found = False
-                            parsing = False
-                            filestring = ""
-              
-                    
-                filestring = filestring + "          " + line
- 
-            if found == True:
-                if (line.find(firstString) >= 0): 
-                    parsing = True
-  
-
-            if line.find(kind) >= 0:
-                found = True
-
-# Create a copy of the cluster operator file and add --- at the end of the file
+# Apply version to the service-version generator
+ytt_command_add_version = "ytt -f ./generators/cluster-service-version-generator.yml --data-value-yaml name=rabbitmq-cluster-operator.v"+version+" --data-value-yaml version="+version+ "> ./tmpmanifests/cluster-service-version-generator.yaml"
+os.system(ytt_command_add_version)
+# Create a copy of the cluster operator and add --- at the end of the file
 cluster_operator_release_file = "./generators/cluster-operator.yaml"
 
 os.system("cp " + operator_release_file + " " + cluster_operator_release_file)
@@ -53,17 +27,19 @@ create_overlay(cluster_operator_release_file, "kind: Role", "rules:", "---", "./
 create_overlay(cluster_operator_release_file, "kind: ClusterRole", "rules:", "---", "./generators/overlay-cluster-permission-generator.yaml", "./overlays/overlay-cluster-permission.yaml")    
 create_overlay(cluster_operator_release_file, "kind: Deployment", "spec:", "terminationGracePeriodSeconds", "./generators/overlay-deployment-generator.yaml", "./overlays/overlay-deployment.yaml")  
 
-# Apply the overlay and generate the ClusterServiceVersion file
-os.system("ytt -f ./overlays/overlay-permission.yaml -f ./generators/cluster-service-version-generator.yml  > ./tmpmanifests/cluster-service-version-permission.yaml")
+# Apply the overlay and generate the ClusterServiceVersion file from overlay
+os.system("ytt -f ./overlays/overlay-permission.yaml -f ./tmpmanifests/cluster-service-version-generator.yaml  > ./tmpmanifests/cluster-service-version-permission.yaml")
 os.system("ytt -f ./overlays/overlay-cluster-permission.yaml -f ./tmpmanifests/cluster-service-version-permission.yaml > ./tmpmanifests/cluster-service-version-cluster-permission.yaml")
 os.system("ytt -f ./overlays/overlay-deployment.yaml -f ./tmpmanifests/cluster-service-version-cluster-permission.yaml > ./generators/cluster-service-version.yaml")
 
-# Create the structure
+# Create the bundle structure
 rabbitmq_cluster_operator_dir="./../../OLM/rabbitmq-cluster-operator/" + version 
+rabbitmq_cluster_operator_dir_manifests=rabbitmq_cluster_operator_dir+"/manifests"
 os.system("mkdir -p ./../../OLM/rabbitmq-cluster-operator/" + version + "/manifests")
-os.system("cp ./generators/cluster-service-version.yaml " + rabbitmq_cluster_operator_dir+"/manifests")
-os.system("cp ./generators/cluster-service-version.yaml " + rabbitmq_cluster_operator_dir+"/manifests")
-os.system("cp ./manifests_crds/crds.yaml " + rabbitmq_cluster_operator_dir+"/manifests")
+os.system("cp ./generators/cluster-service-version.yaml " + rabbitmq_cluster_operator_dir_manifests)
+os.system("cp ./generators/cluster-service-version.yaml " + rabbitmq_cluster_operator_dir_manifests)
+os.system("cp ./generators/bundle.Dockerfile " + rabbitmq_cluster_operator_dir)
+os.system("cp ./manifests_crds/crds.yaml " + rabbitmq_cluster_operator_dir_manifests)
 os.system("cp -fR ./metadata/metadata " + rabbitmq_cluster_operator_dir)
 
 
