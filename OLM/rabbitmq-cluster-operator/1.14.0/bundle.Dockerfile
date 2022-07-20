96a21f7d6994b251655645a7d52df1265cd58124
DanielePalaia <daniele985@gmail.com>
2022-07-20 11:31:21 +0200

    commit

--- OLM/rabbitmq-cluster-operator/1.14.0/bundle.Dockerfile
+++ OLM/rabbitmq-cluster-operator/1.14.0/bundle.Dockerfile
@@ -5,5 +5,7 @@ LABEL operators.operatorframework.io.bundle.manifests.v1=manifests/
 LABEL operators.operatorframework.io.bundle.metadata.v1=metadata/
 LABEL operators.operatorframework.io.bundle.package.v1=rabbitmq-operator
 LABEL operators.operatorframework.io.bundle.channels.v1=stable
-COPY ../rabbitmq-cluster-operator/1.14.0/manifests /manifests/
-COPY ../rabbitmq-cluster-operator/1.14.0/metadata /metadata/
+LABEL operators.operatorframework.io.bundle.channel.default.v1=stable
+
+COPY ../../OLM/rabbitmq-cluster-operator/1.14.0/manifests /manifests/
+COPY ../../OLM/rabbitmq-cluster-operator/1.14.0/metadata /metadata/
--- generate_OLM/generate_OLM_cluster_operator/generate-olm-package.py
+++ generate_OLM/generate_OLM_cluster_operator/generate-olm-package.py
@@ -6,12 +6,13 @@ sys.path.insert(1, './../../common_code')
 
 from create_overlay import create_overlay
 
-if len(sys.argv) == 2:
-   print('the script needs at least a cluster operator manifests and version as arguments in order to create the OLM structure')
+if len(sys.argv) == 3:
+   print('the script needs at least a cluster operator manifests a version and the output directory where store the Bundle as arguments in order to create the OLM structure')
    sys.exit()
 
 operator_release_file = sys.argv[1] 
 version = sys.argv[2]
+output_directory = sys.argv[3]
 
 # Apply version to the service-version generator
 ytt_command_add_version = "ytt -f ./generators/cluster-service-version-generator.yml --data-value-yaml name=rabbitmq-cluster-operator.v"+version+" --data-value-yaml version="+version+ "> ./tmpmanifests/cluster-service-version-generator.yaml"
@@ -32,14 +33,18 @@ os.system("ytt -f ./overlays/overlay-permission.yaml -f ./tmpmanifests/cluster-s
 os.system("ytt -f ./overlays/overlay-cluster-permission.yaml -f ./tmpmanifests/cluster-service-version-permission.yaml > ./tmpmanifests/cluster-service-version-cluster-permission.yaml")
 os.system("ytt -f ./overlays/overlay-deployment.yaml -f ./tmpmanifests/cluster-service-version-cluster-permission.yaml > ./generators/cluster-service-version.yaml")
 
+
 # Create the bundle structure
-rabbitmq_cluster_operator_dir="./../../OLM/rabbitmq-cluster-operator/" + version 
+rabbitmq_cluster_operator_dir=output_directory+"/" + version 
 rabbitmq_cluster_operator_dir_manifests=rabbitmq_cluster_operator_dir+"/manifests"
-os.system("mkdir -p ./../../OLM/rabbitmq-cluster-operator/" + version + "/manifests")
+os.system("mkdir -p " + rabbitmq_cluster_operator_dir_manifests)
 os.system("cp ./generators/cluster-service-version.yaml " + rabbitmq_cluster_operator_dir_manifests)
-os.system("cp ./generators/bundle.Dockerfile " + rabbitmq_cluster_operator_dir)
 os.system("cp ./manifests_crds/crds.yaml " + rabbitmq_cluster_operator_dir_manifests)
-os.system("cp -fR ./metadata/metadata " + rabbitmq_cluster_operator_dir)
+
+# Generate metadata/annotations and Dockerfile
+os.system("opm alpha bundle generate --directory " + rabbitmq_cluster_operator_dir_manifests + " --package rabbitmq-operator --channels stable --default stable")
+os.system("mv ./bundle.Dockerfile " + rabbitmq_cluster_operator_dir)
+
 
 
 
